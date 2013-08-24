49daa6a9d5e0ffa68a6c65ba4d3e4e64dcdb2401
Paulo Coutinho <paulo@prsolucoes.com>
2013-08-24 11:29:37 -0300

    basic player is working, moving and jumping

---
+++ Classes/Player.cpp
@@ -0,0 +1,9 @@
+//
+//  Player.cpp
+//  JoystickCocos2D
+//
+//  Created by Paulo Coutinho on 8/24/13.
+//
+//
+
+#include "Player.h"
---
+++ Classes/Player.h
@@ -0,0 +1,14 @@
+//
+//  Player.h
+//  JoystickCocos2D
+//
+//  Created by Paulo Coutinho on 8/24/13.
+//
+//
+
+#ifndef __JoystickCocos2D__Player__
+#define __JoystickCocos2D__Player__
+
+#include <iostream>
+
+#endif /* defined(__JoystickCocos2D__Player__) */
