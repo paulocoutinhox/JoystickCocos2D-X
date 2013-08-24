4a68033fc0e7a42ea144526a070d09b9b4e2c134
Paulo Coutinho <paulo@prsolucoes.com>
2013-08-24 07:19:56 -0300

    new version working iOS, OSX and Android

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -8,7 +8,15 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/HelloWorldScene.cpp \
+                   ../../Classes/SneakyInput/SneakyButton.cpp \
+                   ../../Classes/SneakyInput/SneakyButton.h \
+                   ../../Classes/SneakyInput/SneakyButtonSkinnedBase.cpp \
+                   ../../Classes/SneakyInput/SneakyButtonSkinnedBase.h \
+				   ../../Classes/SneakyInput/SneakyJoystick.cpp \
+				   ../../Classes/SneakyInput/SneakyJoystick.h \
+				   ../../Classes/SneakyInput/SneakyJoystickSkinnedBase.cpp \
+				   ../../Classes/SneakyInput/SneakyJoystickSkinnedBase.h
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
